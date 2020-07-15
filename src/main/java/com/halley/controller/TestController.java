990bbc7171815a7962e2c894a0f3f976f0f49d52
Joecpp <319405596@qq.com>
2020-07-15 22:52:50 +0800

    SpringSpringMVC完成

--- src/main/java/com/halley/controller/TestController.java
+++ src/main/java/com/halley/controller/TestController.java
@@ -1,8 +1,31 @@
 package com.halley.controller;
 
-/**
- * @author Jan
- * @Date 2020/7/14 11:51
- */
+import com.halley.bean.Dog;
+import com.halley.bean.TestBean;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping("/The_best_project_war_exploded")
 public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(ModelMap modelMap){
+        modelMap.addAttribute("name","Hello World");
+        return "list";
+    }
+
+    @RequestMapping("/json")
+    @ResponseBody
+    public TestBean josn(){
+
+        return new TestBean("乔云乐",200,new Dog("wang",2));
+    }
 }
